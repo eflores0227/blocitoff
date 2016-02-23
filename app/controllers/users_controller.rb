0d811941ced575a81fc5fc4b7686f7e380650149
Estefania Flores <eflores0227@gmail.com>
2016-02-22 19:53:11 -0800

    signing in and out

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      flash[:notice] = "Welcome to Blocipedia #{@user.name}"
+      create_session(@user)
+      redirect_to root path
+    else
+      flash.now[:alert] = "There was an error creating you account. Please try again."
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+  end
+end
